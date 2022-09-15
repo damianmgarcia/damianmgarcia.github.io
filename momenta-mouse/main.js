16558ade9cccfdafc7f75edeb22d4766c0619359
Damian Michael Garcia <damian@outlook.com>
2022-09-14 23:05:20 -0500

    Add Combination Scroller and Kitteh Blink Logic
    (1) A combination scroller was added to the demo.
    
    (2) My kitteh logo was added at the end, and it blinks.

--- momenta-mouse/main.js
+++ momenta-mouse/main.js
@@ -1,28 +1,93 @@
 import { getDeviceHeuristics } from "/scripts/modules/utilities.js";
 import { MomentaMouse } from "/scripts/modules/momenta-mouse.js";
+import { SmoothScroller } from "/scripts/modules/smooth-scroller.js";
 
 document.querySelector(".date-year").textContent = new Date().getFullYear();
 
-let hue = -5;
-document
-  .querySelectorAll(".carousel-item")
-  .forEach((element) =>
-    element.style.setProperty("background", `hsl(${(hue += 5)}, 50%, 50%)`)
-  );
-
-let number = 0;
-document.querySelectorAll(".carousel-item").forEach((element) => {
+const createAndAddCarouselItemNumberDiv = (element, number) => {
   const carouselItemNumber = document.createElement("div");
   carouselItemNumber.classList.add("carousel-item-number");
-  carouselItemNumber.textContent = number += 1;
+  carouselItemNumber.textContent = number;
   element.insertAdjacentElement("afterbegin", carouselItemNumber);
+};
+
+document.querySelectorAll(".carousel.vertical").forEach((verticalCarousel) => {
+  let hue = 0;
+  let number = 0;
+
+  verticalCarousel
+    .querySelectorAll(".carousel-item")
+    .forEach((carouselItem) => {
+      carouselItem.style.setProperty("background", `hsl(${hue}, 50%, 50%)`);
+      hue += 27;
+      createAndAddCarouselItemNumberDiv(carouselItem, number++);
+    });
 });
+
+let hue = 0;
+let number = 0;
+document
+  .querySelectorAll(".carousel.horizontal > .carousel-item")
+  .forEach((carouselItem) => {
+    carouselItem.style.setProperty("background", `hsl(${hue}, 50%, 50%)`);
+    hue += 5;
+    createAndAddCarouselItemNumberDiv(carouselItem, number++);
+  });
 
 const { isTouchScreen } = getDeviceHeuristics();
 if (!isTouchScreen) {
   MomentaMouse.autoCreateScrollers({
     considerOverflowHiddenAxesNonScrollable: false,
+    selectorsToIgnore: [".carousel-container, svg"],
+  }).setSelectorsOfClickableElements([".kitteh"]);
+
+  const kitteh = document.querySelector(".kitteh");
+  kitteh.addEventListener("pointerdown", () => {
+    kitteh.querySelectorAll(".eyelid-down").forEach((element) => {
+      element.beginElement();
+    });
   });
+
+  const openEyes = () => {
+    kitteh.querySelectorAll(".eyelid-up").forEach((element) => {
+      element.beginElement();
+    });
+  };
+
+  ["pointercancel", "pointerup"].forEach((eventType) =>
+    kitteh.addEventListener(eventType, () => openEyes())
+  );
+
+  ["contextmenu", "momentaMouseScrollerPointerRoute"].forEach((eventType) =>
+    document.addEventListener(eventType, (event) => {
+      if (
+        event.type === "momentaMouseScrollerPointerRoute" &&
+        event.detail.routeFrom !== kitteh
+      )
+        return;
+
+      openEyes();
+    })
+  );
+
+  addEventListener("blur", () => openEyes());
+
+  document
+    .querySelector(".combination-reset-button")
+    .addEventListener("click", () => {
+      let wait = 0;
+      document.querySelectorAll(".carousel.vertical").forEach((scroller) => {
+        setTimeout(
+          () =>
+            SmoothScroller.scroll({
+              scrollContainer: scroller,
+              y: 0,
+            }),
+          wait
+        );
+        wait += 34;
+      });
+    });
 
   document.querySelectorAll("select").forEach((element) =>
     element.addEventListener("input", (event) => {
@@ -65,6 +130,50 @@ if (!isTouchScreen) {
       }
     })
   );
+
+  const snapCarouselItemIntoAlignment = (verticalCarousel) => {
+    if (!verticalCarousel.matches(".vertical")) return;
+
+    const verticalCarouselScrollTop = verticalCarousel.scrollTop;
+
+    const verticalCarouselItemOffsets = Array.from(
+      verticalCarousel.querySelectorAll(".carousel-item")
+    ).map(
+      (carouseItem) =>
+        carouseItem.offsetTop -
+        Number.parseInt(getComputedStyle(carouseItem).marginTop)
+    );
+
+    const carouselItemDistancesFromScrollTop = verticalCarouselItemOffsets.map(
+      (offset) => Math.abs(offset - verticalCarouselScrollTop)
+    );
+
+    const smallestDistanceFromScrollTop = Math.min(
+      ...carouselItemDistancesFromScrollTop
+    );
+
+    const carouselItemClosestToScrollTop =
+      carouselItemDistancesFromScrollTop.indexOf(smallestDistanceFromScrollTop);
+
+    SmoothScroller.scroll({
+      scrollContainer: verticalCarousel,
+      y: verticalCarouselItemOffsets[carouselItemClosestToScrollTop],
+    });
+  };
+
+  document.addEventListener("momentaMouseScrollerPointerRoute", (event) => {
+    if (event.detail.routeFrom && event.detail.routeFrom.matches(".vertical"))
+      snapCarouselItemIntoAlignment(event.detail.routeFrom);
+  });
+
+  document.addEventListener("momentaMouseScrollerScrollStop", (event) => {
+    const scrollWasInterrupted =
+      event.detail.interruptedBy !==
+        "Scroll distance is below minimum scrollable distance" &&
+      event.detail.interruptedBy;
+    if (!scrollWasInterrupted)
+      snapCarouselItemIntoAlignment(event.detail.scrollContainer);
+  });
 } else if (isTouchScreen) {
   document
     .querySelector(".touch-screen-modal")
