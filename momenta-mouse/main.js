661182c0136e653415d63bd7ac0e665c1f3556a4
Damian Michael Garcia <damian@outlook.com>
2022-09-23 22:49:33 -0500

    Allow Touch-Screen Devices
    (1) Previously, a full-screen modal element would display if a
    touch-screen device was detected, with a message asking the user to
    return on a non-touch-screen device with a mouse. This is no longer
    how MomentaMouse works. Now, as long as a mouse is detected, it works.
    It even works on touch-screens if a mouse is available. The page now
    uses CSS media queries as heuristics to identify if a fine, hoverable
    pointer is available. Although this is not mouse-specific (it may also
    accept touchpads), it is the best solution for now. When a mouse is
    found, MomentaMouse automatically activates. When a mouse is not found,
    MomentaMouse disables itself and the page updates to reflect this.

--- momenta-mouse/main.js
+++ momenta-mouse/main.js
@@ -1,6 +1,6 @@
-import { getDeviceHeuristics } from "/scripts/modules/utilities.js";
 import { MomentaMouse } from "/scripts/modules/momenta-mouse.js";
 import { SmoothScroller } from "/scripts/modules/smooth-scroller.js";
+import { Heuristics } from "/scripts/modules/utilities.js";
 
 document.querySelector(".date-year").textContent = new Date().getFullYear();
 
@@ -18,7 +18,7 @@ document.querySelectorAll(".carousel.vertical").forEach((verticalCarousel) => {
   verticalCarousel
     .querySelectorAll(".carousel-item")
     .forEach((carouselItem) => {
-      carouselItem.style.setProperty("background", `hsl(${hue}, 50%, 50%)`);
+      carouselItem.style.setProperty("background", `hsl(${hue}, 100%, 50%)`);
       hue += 27;
       createAndAddCarouselItemNumberDiv(carouselItem, number++);
     });
@@ -29,160 +29,204 @@ let number = 0;
 document
   .querySelectorAll(".carousel.horizontal > .carousel-item")
   .forEach((carouselItem) => {
-    carouselItem.style.setProperty("background", `hsl(${hue}, 50%, 50%)`);
+    carouselItem.style.setProperty("background", `hsl(${hue}, 100%, 50%)`);
     hue += 5;
     createAndAddCarouselItemNumberDiv(carouselItem, number++);
   });
 
-const { isTouchScreen } = getDeviceHeuristics();
-if (!isTouchScreen) {
-  MomentaMouse.autoCreateScrollers({
-    considerOverflowHiddenAxesNonScrollable: false,
-    selectorsToIgnore: [".carousel-container, svg"],
-  }).setSelectorsOfClickableElements([".kitteh"]);
+const kitteh = document.querySelector(".kitteh");
+kitteh.addEventListener("pointerdown", () => {
+  kitteh.querySelectorAll(".eyelid-down").forEach((element) => {
+    element.beginElement();
+  });
+});
 
-  const kitteh = document.querySelector(".kitteh");
-  kitteh.addEventListener("pointerdown", () => {
-    kitteh.querySelectorAll(".eyelid-down").forEach((element) => {
-      element.beginElement();
+const openEyes = () => {
+  kitteh.querySelectorAll(".eyelid-up").forEach((element) => {
+    element.beginElement();
+  });
+};
+
+["pointercancel", "pointerup"].forEach((eventType) =>
+  kitteh.addEventListener(eventType, () => openEyes())
+);
+
+["contextmenu", "momentaMouseScrollerPointerRoute"].forEach((eventType) =>
+  document.addEventListener(eventType, (event) => {
+    if (
+      event.type === "momentaMouseScrollerPointerRoute" &&
+      event.detail.routeFrom !== kitteh
+    )
+      return;
+
+    openEyes();
+  })
+);
+
+addEventListener("blur", () => openEyes());
+
+document
+  .querySelector(".combination-reset-button")
+  .addEventListener("click", () => {
+    let wait = 0;
+    document.querySelectorAll(".carousel.vertical").forEach((scroller) => {
+      setTimeout(
+        () =>
+          SmoothScroller.scroll({
+            scrollContainer: scroller,
+            y: 0,
+          }),
+        wait
+      );
+      wait += 34;
     });
   });
 
-  const openEyes = () => {
-    kitteh.querySelectorAll(".eyelid-up").forEach((element) => {
-      element.beginElement();
-    });
-  };
-
-  ["pointercancel", "pointerup"].forEach((eventType) =>
-    kitteh.addEventListener(eventType, () => openEyes())
-  );
-
-  ["contextmenu", "momentaMouseScrollerPointerRoute"].forEach((eventType) =>
-    document.addEventListener(eventType, (event) => {
-      if (
-        event.type === "momentaMouseScrollerPointerRoute" &&
-        event.detail.routeFrom !== kitteh
-      )
-        return;
-
-      openEyes();
-    })
-  );
-
-  addEventListener("blur", () => openEyes());
-
-  document
-    .querySelector(".combination-reset-button")
-    .addEventListener("click", () => {
-      let wait = 0;
-      document.querySelectorAll(".carousel.vertical").forEach((scroller) => {
-        setTimeout(
-          () =>
-            SmoothScroller.scroll({
-              scrollContainer: scroller,
-              y: 0,
-            }),
-          wait
-        );
-        wait += 34;
-      });
-    });
-
-  document.querySelectorAll("select").forEach((element) =>
-    element.addEventListener("input", (event) => {
-      const option = event.target.name;
-      const value = event.target.value;
-      if (option === "deceleration") {
+document.querySelectorAll("select").forEach((element) =>
+  element.addEventListener("input", (event) => {
+    const option = event.target.name;
+    const value = event.target.value;
+    if (option === "deceleration") {
+      MomentaMouse.getAllScrollers().forEach((scroller) =>
+        scroller.setDecelerationLevel(value)
+      );
+    } else if (option === "bounciness") {
+      MomentaMouse.getAllScrollers().forEach((scroller) =>
+        scroller.setBorderBouncinessLevel(value)
+      );
+    } else if (option === "activation-state") {
+      if (value === "on") {
         MomentaMouse.getAllScrollers().forEach((scroller) =>
-          scroller.setDecelerationLevel(value)
+          scroller.activate()
         );
-      } else if (option === "bounciness") {
+      } else if (value === "off") {
         MomentaMouse.getAllScrollers().forEach((scroller) =>
-          scroller.setBorderBouncinessLevel(value)
+          scroller.deactivate()
         );
-      } else if (option === "activation-state") {
-        if (value === "on") {
-          MomentaMouse.getAllScrollers().forEach((scroller) =>
-            scroller.activate()
-          );
-        } else if (value === "off") {
-          MomentaMouse.getAllScrollers().forEach((scroller) =>
-            scroller.deactivate()
-          );
-        }
-      } else if (option === "reactive-cursor") {
-        if (value === "on") {
-          MomentaMouse.getAllScrollers().forEach((scroller) =>
-            scroller.setAllowReactiveCursor(true)
-          );
-        } else if (value === "off") {
-          MomentaMouse.getAllScrollers().forEach((scroller) =>
-            scroller.setAllowReactiveCursor(false)
-          );
-        }
-      } else if (option === "quick-toggle-key") {
-        if (value === "on") {
-          MomentaMouse.setAllowQuickToggleKey(true);
-        } else if (value === "off") {
-          MomentaMouse.setAllowQuickToggleKey(false);
-        }
       }
-    })
-  );
-
-  const snapCarouselItemIntoAlignment = (verticalCarousel) => {
-    if (!verticalCarousel.matches(".vertical")) return;
-
-    const verticalCarouselScrollTop = verticalCarousel.scrollTop;
-
-    const verticalCarouselItemOffsets = Array.from(
-      verticalCarousel.querySelectorAll(".carousel-item")
-    ).map(
-      (carouseItem) =>
-        carouseItem.offsetTop -
-        Number.parseInt(getComputedStyle(carouseItem).marginTop)
-    );
-
-    const carouselItemDistancesFromScrollTop = verticalCarouselItemOffsets.map(
-      (offset) => Math.abs(offset - verticalCarouselScrollTop)
-    );
-
-    const smallestDistanceFromScrollTop = Math.min(
-      ...carouselItemDistancesFromScrollTop
-    );
-
-    const carouselItemClosestToScrollTop =
-      carouselItemDistancesFromScrollTop.indexOf(smallestDistanceFromScrollTop);
-
-    SmoothScroller.scroll({
-      scrollContainer: verticalCarousel,
-      y: verticalCarouselItemOffsets[carouselItemClosestToScrollTop],
-    });
-  };
-
-  document.addEventListener(
-    "momentaMouseScrollerPointerHandlingStop",
-    (event) => {
-      if (!event.detail.scrollContainer.matches(".vertical")) return;
-
-      if (event.detail.interruptedBy)
-        snapCarouselItemIntoAlignment(event.detail.scrollContainer);
+    } else if (option === "reactive-cursor") {
+      if (value === "on") {
+        MomentaMouse.getAllScrollers().forEach((scroller) =>
+          scroller.setAllowReactiveCursor(true)
+        );
+      } else if (value === "off") {
+        MomentaMouse.getAllScrollers().forEach((scroller) =>
+          scroller.setAllowReactiveCursor(false)
+        );
+      }
+    } else if (option === "quick-toggle-key") {
+      if (value === "on") {
+        MomentaMouse.setAllowQuickToggleKey(true);
+      } else if (value === "off") {
+        MomentaMouse.setAllowQuickToggleKey(false);
+      }
     }
+  })
+);
+
+const snapCarouselItemIntoAlignment = (verticalCarousel) => {
+  if (!verticalCarousel.matches(".vertical")) return;
+
+  const verticalCarouselScrollTop = verticalCarousel.scrollTop;
+
+  const verticalCarouselItemOffsets = Array.from(
+    verticalCarousel.querySelectorAll(".carousel-item")
+  ).map(
+    (carouseItem) =>
+      carouseItem.offsetTop -
+      Number.parseInt(getComputedStyle(carouseItem).marginTop)
   );
 
-  document.addEventListener("momentaMouseScrollerScrollStop", (event) => {
+  const carouselItemDistancesFromScrollTop = verticalCarouselItemOffsets.map(
+    (offset) => Math.abs(offset - verticalCarouselScrollTop)
+  );
+
+  const smallestDistanceFromScrollTop = Math.min(
+    ...carouselItemDistancesFromScrollTop
+  );
+
+  const carouselItemClosestToScrollTop =
+    carouselItemDistancesFromScrollTop.indexOf(smallestDistanceFromScrollTop);
+
+  SmoothScroller.scroll({
+    scrollContainer: verticalCarousel,
+    y: verticalCarouselItemOffsets[carouselItemClosestToScrollTop],
+  });
+};
+
+const selectElements = document.querySelectorAll("select");
+const activationStateSelect = document.querySelector(
+  "#activation-state-select"
+);
+
+const applyMouseFoundChanges = () => {
+  selectElements.forEach((selectElement) =>
+    selectElement.removeAttribute("disabled")
+  );
+  activationStateSelect.value = "on";
+};
+
+const applyMouseNotFoundChanges = () => {
+  selectElements.forEach((selectElement) =>
+    selectElement.setAttribute("disabled", "disabled")
+  );
+  activationStateSelect.value = "off";
+};
+
+const updatePageAccordingToMouseAvailability = (hasMouseOrTouchpad) => {
+  if (hasMouseOrTouchpad) {
+    applyMouseFoundChanges();
+  } else if (!hasMouseOrTouchpad) {
+    applyMouseNotFoundChanges();
+  }
+};
+
+const { hasMouseOrTouchpad } = Heuristics.getDeviceHeuristics({
+  listenForAndDispatchChanges: true,
+});
+updatePageAccordingToMouseAvailability(hasMouseOrTouchpad);
+
+document.addEventListener("deviceHeuristicsChange", (event) => {
+  if (event.detail.property !== "hasMouseOrTouchpad") return;
+
+  const { newValue: hasMouseOrTouchpad } = event.detail;
+  updatePageAccordingToMouseAvailability(hasMouseOrTouchpad);
+});
+
+document.addEventListener("momentaMouseScrollerActivate", (event) => {
+  if (event.detail.scrollContainer !== document.documentElement) return;
+
+  document.body.dataset.momentaMouseActive = "true";
+});
+
+document.addEventListener("momentaMouseScrollerDeactivate", (event) => {
+  if (event.detail.scrollContainer !== document.documentElement) return;
+
+  document.body.dataset.momentaMouseActive = "false";
+});
+
+document.addEventListener(
+  "momentaMouseScrollerPointerHandlingStop",
+  (event) => {
     if (!event.detail.scrollContainer.matches(".vertical")) return;
 
-    const scrollWasInterrupted =
-      event.detail.interruptedBy !==
-        "Scroll distance is below minimum scrollable distance" &&
-      event.detail.interruptedBy;
-    if (!scrollWasInterrupted)
+    if (event.detail.interruptedBy)
       snapCarouselItemIntoAlignment(event.detail.scrollContainer);
-  });
-} else if (isTouchScreen) {
-  document
-    .querySelector(".touch-screen-modal")
-    .style.setProperty("display", "grid");
-}
+  }
+);
+
+document.addEventListener("momentaMouseScrollerScrollStop", (event) => {
+  if (!event.detail.scrollContainer.matches(".vertical")) return;
+
+  const scrollWasInterrupted =
+    event.detail.interruptedBy !==
+      "Scroll distance is below minimum scrollable distance" &&
+    event.detail.interruptedBy;
+  if (!scrollWasInterrupted)
+    snapCarouselItemIntoAlignment(event.detail.scrollContainer);
+});
+
+MomentaMouse.autoCreateScrollers({
+  considerOverflowHiddenAxesNonScrollable: false,
+  selectorsToIgnore: [".carousel-container, svg"],
+}).setSelectorsOfClickableElements([".kitteh"]);
